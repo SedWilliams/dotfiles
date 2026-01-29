1ced96f62335e6d084819024b8ad677413a0f234
tre3iii <lilshed3@gmail.com>
2026-01-28 19:50:54 -0800

    Create master install script for dotfiles
    Add master install script for dotfiles with placeholder comments.

---
+++ install_scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is the master install script which installs all of the dotfiles from the repo.
+# UIMPLEMENTED, add logic to check for existing installations and also error handling to fail safely
+# Optionally add optional install selections
