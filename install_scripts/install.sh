7a0c69bb3278a404117613487256424cc9c24367
tre3iii <lilshed3@gmail.com>
2026-02-06 21:12:14 -0800

    Update install script to include Gemini CLI and Node.js
    Added execution permissions and sourced installation scripts for Gemini CLI and Node.js.

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
 # This script is the master install script which installs all of the dotfiles from the repo.
-# UIMPLEMENTED, add logic to check for existing installations and also error handling to fail safely
-# Optionally add optional install selections
+
+chmod a+x ./install_gemini_cli.sh
+source ./install_gemini_cli.sh
+
+chmod a+x ./install_node.sh
+source ./install_node.sh
+
+echo "Please close and reopen your terminal or run 'source ~/.bashrc' (or ~/.zshrc) to start using Node.js."
