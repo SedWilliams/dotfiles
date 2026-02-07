05975d4956672494a21c141b43f240c7995c9e3c
tre3iii <lilshed3@gmail.com>
2026-02-06 21:00:08 -0800

    Update and rename install_gemini-cli.sh to install_gemini_cli.sh

--- install_scripts/install_gemini_cli.sh
+++ install_scripts/install_gemini_cli.sh
@@ -2,15 +2,12 @@
 #!/bin/bash
 
 echo "Checking to see if gemini-cli is installed..."
-
 INSTALL_PATH=$(command -v gemini)
 
-if [[ -z "$INSTALL_PATH" ]]; then
+if [ -z $INSTALL_PATH ]; then
   npm install -g @google/gemini-cli
   echo "gemini-cli installation complete."
 else
   echo "gemini-cli is already installed..."
   echo $INSTALL_PATH
 fi
-
-
