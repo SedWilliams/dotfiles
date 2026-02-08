732bfdd8e4effc0a5e87b0328bde238aaa0a7a4e
tre3iii <lilshed3@gmail.com>
2026-02-08 12:10:51 -0800

    reformat install scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script is the master install script which installs all of the dotfiles from the repo.
 
-chmod a+x ./install_gemini_cli.sh
-source ./install_gemini_cli.sh
+chmod a+x ./install_scripts/install_gemini_cli.sh
+source ./install_scripts/install_gemini_cli.sh
 
-chmod a+x ./install_node.sh
-source ./install_node.sh
+chmod a+x ./install_scripts/install_node.sh
+source ./install_scripts/install_node.sh
 
 echo "Please close and reopen your terminal or run 'source ~/.bashrc' (or ~/.zshrc) to start using Node.js."
